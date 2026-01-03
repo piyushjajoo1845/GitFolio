a21a7a563313d6982f1bcaf2b3ce198f4d12bfba
Piyush Jajoo <piyushjajo04@gmail.com>
2026-01-03 23:38:29 +0530

    Adding repo fetching and profile fetching functions

--- gitfolio/src/services/githubApi.js
+++ gitfolio/src/services/githubApi.js
@@ -1,12 +1,42 @@
-const BASE_URL = "https://api.github.com";
 
 
-export async function fetchGitHubUser(username){
-    const response = await fetch(`${BASE_URL}/users/${username}`);
+async function fetchUserProfile(username){
+    try{
+        const url = `https://api.github.com/users/${username}`;
 
-    if(!response.ok){
-        throw new Error("Github user not found");
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error('User not found: ${username}');
+        }
+        const data = await response.json();
+
+        return data;
+
+    } catch (error){
+            console.error("Error fetching user profile:" , error.message);
+            throw error;
     }
-
-    return response.json();
+    
 }
+
+
+async function fetchUserRepos(username){
+    try{
+        const url = `https://api.github.com/users/${username}/repos?per_page=30&sort=updated`;
+
+        const response = await fetch(url);
+
+        if(!response.ok){
+            throw new Error(`Failed to fetch repos for ${username}`);
+        }
+
+        const repos = await response.json();
+
+        return repos;
+    }catch(error){
+        console.error("Error fetching user repos:" , error.message);
+        throw error;
+    }
+}
+
+export { fetchUserProfile  , fetchUserRepos};
